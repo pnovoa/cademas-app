39ced6e4cf928ceafe541baf3c56f57462c913cd
Pavel Novoa <pavel.novoa@gmail.com>
2026-06-18 11:41:42 +0100

    Update about tab in app_v1.py: Added a blank line for improved readability in the funding section.

--- app/app_v1.py
+++ app/app_v1.py
@@ -186,6 +186,7 @@ def render_about_tab():
         - **Pavel Novoa Hernández** (Universidad de La Laguna, Spain) — pnovoahe@ull.edu.es
         - Mariia Godz (Universidad de Granada, Spain) — mariiagodz@ugr.es
         - David A. Pelta (Universidad de Granada, Spain) — dpelta@ugr.es
+        
         **Funding:**
         This application has been funded by the project:
         *Study, Analysis and Evaluation of Cooperative Automated Decision-Making Systems (CADEMAS)* \
